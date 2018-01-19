69c9bdf4222cd57f2965cb73c1bce736c0f1bbc4
Chris <cmanswanner@gmail.com>
2018-01-19 09:26:38 -0800

    add variables and start of for loop into javascript

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#inputForm").submit(function(event){
+    event.preventDefault();
+var userNumber = $("#inputNumber").val();
+var returnArray = [];
+
+for (var i = 0; i <= userNumber; i++) {
+  var index = String(i);
+  if (!(i %))
+}
