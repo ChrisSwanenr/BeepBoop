7ec111917a292d66b9c428606bf7381edd1b864b
Chris <cmanswanner@gmail.com>
2018-01-19 12:07:41 -0800

    move beepBoop function outside of click event to fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
+function beepBoop(userNum) {
+  if (userNum >= 1) {
+    var returnArray = [];
+    var expression1 = /1/g;
+    var expression2 = /0/g;
+    for (var i = 1; i <= userNum; i++) {
+      var stringIndex = String(i);
+      if (!(i % 3)) {
+        console.log(returnArray);
+        returnArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (expression2.test(stringIndex)) {
+        returnArray.push("Boop!");
+        console.log(returnArray);
+      } else if (expression1.test(stringIndex)) {
+        returnArray.push("Beep!");
+      } else {
+        returnArray.push(i);
+      }
+    }
+    return returnArray;
+    console.log(returnArray);
+  }
+  return false;
+}
 $(document).ready(function(){
-  $("#button").click(function(event){
-    debugger;
-
+  $("#button").click(function(event) {
     var userNum = parseInt($("#userInput").val());
     if (beepBoop(userNum).length > 50) {
       $("#displayP").text("That is a long number!");
@@ -10,30 +32,5 @@ $(document).ready(function(){
         $("#displayP").text(each);
       })
     }
-    function beepBoop(userNum) {
-      if (userNum >= 1) {
-        var returnArray = [];
-        var expression1 = /1/g;
-        var expression2 = /0/g;
-        for (var i = 1; i <= userNum; i++) {
-          var stringIndex = String(i);
-          if (!(i % 3)) {
-            console.log(returnArray);
-            returnArray.push("I'm sorry, Dave. I'm afraid I can't do that.")
-          } else if (expression1.test(stringIndex)) {
-            returnArray.push("Boop!");
-            console.log(returnArray);
-          } else if (expression2.test(stringIndex)) {
-            returnArray.push("Beep!");
-          } else {
-            returnArray.push(i);
-          }
-        }
-        return returnArray;
-        console.log(returnArray);
-      }
-      return false;
-    }
-
   })
 })
