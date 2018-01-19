718ac0d43fe8e7e3b654f64c03d5038ef32c28a3
Chris <cmanswanner@gmail.com>
2018-01-19 10:46:24 -0800

    retry scripts page, bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
   $("#inputForm").submit(function(event){
     event.preventDefault();
-var userNumber = $("#inputNumber").val();
-var returnArray = [];
+    debugger;
+  function count(index) {
+    for (var i = 0; i <= index; i++) {
+      numberArray.push(i);
 
-for (var i = 0; i <= userNumber; i++) {
-  var index = String(i);
-  if (!(i %))
+    var userInput = parseInt($("#inputNumber").val());
+    var numberArray = []
+    var result = count(userInput);
+  var switchedWords = numberArray.map(function(number){
+      if (number % 3 === 0) {
+        return "I'm sorry, Dave. I'm afraid I can't let you do that";
+      } else if (/1/.test(number)) {
+        return "Beep!";
+      } else if (/0/.test(number)) {
+        return "Boop!";
+      } else {
+        return "Whoops!";
+      }
+    });
+    return switchedWords;
+    console.log(switchedWords)
+  }
 }
+})
+})
